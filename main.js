e81e3d0801e5362c9ca5530a01a6bad63d7b0b10
Gabriele Gussoni <g-flex@Gabrieles-Mini.local>
2021-12-15 12:47:56 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Vue explorer',
+            description: 'Simple js btc explorer made with vue.js'
+        }
+    }
+});
