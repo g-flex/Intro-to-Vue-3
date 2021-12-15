bc1e90e2bc4017ba15a6e0c7fcdc1a257eec7192
Gabriele Gussoni <g-flex@Gabrieles-Mini.local>
2021-12-15 18:53:55 +0100

    update

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ const app = Vue.createApp({
             description: ()=> { return 'Simple js ' + (this.isBtc === true ? 'btc' : this.isBtc) + ' explorer made with vue.js' },
             imgSrc: './assets/images/socks_blue.jpg',
             supportedCryptos: [
-                { name: 'bitcoin', symbol: 'BTC' },
-                { name: 'solana', symbol: 'SOL' }
+                { name: 'bitcoin', symbol: 'BTC', placeholder: 'bc1000aaa000aaa000aaa000', fn: 'getBtcAddress' },
+                { name: 'solana', symbol: 'SOL', placeholder: '9LC426VbNywjPjXb6JjxSNzSAZgM', fn: 'getSolAccountTxs' }
             ]
         };
     },
     methods: {
-        submitForm: (e) => {
+        submitForm: (e, fn) => {
             e.preventDefault();
-            this.submitQuery(e.currentTarget, 'getBtcAddress');
+            this.submitQuery(e.currentTarget, fn);
         },
         submitQuery: (container, fn) => {
             document.activeElement.blur();
@@ -25,5 +25,10 @@ const app = Vue.createApp({
                 Loader.init().hideTargeted(container);
             });
         }
+    },
+    //to have them in listeners inline
+    computed: {
+        console: () => console,
+        window: () => window
     }
 });
