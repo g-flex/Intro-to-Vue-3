da1887c8765cb5b4d198564ec7d6c94569306c3e
Gabriele Gussoni <g-flex@Gabrieles-Mini.local>
2021-12-16 18:01:25 +0100

    update

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'Vue explorer',
+            //same as in computed, but to be called with (), as a function
+            //description() { return 'Simple js ' + (this.isBtc === true ? 'btc' : this.isBtc) + ' explorer made with vue.js' },
             isBtc: true,
-            description: ()=> { return 'Simple js ' + (this.isBtc === true ? 'btc' : this.isBtc) + ' explorer made with vue.js' },
             imgSrc: './assets/images/socks_blue.jpg',
             supportedCryptos: [
                 { name: 'bitcoin', symbol: 'BTC', placeholder: 'bc1000aaa000aaa000aaa000', fn: 'getBtcAddress' },
@@ -14,10 +15,17 @@ const app = Vue.createApp({
                     borderRadius: '5px',
                     border: '2px solid '+COLORS.txt_1
                 }
-            }
+            },
+            currLoaderName: null,
+            showingLoader: true
         };
     },
     methods: {
+        showLoader(show = true, name = '') {
+            console.log(this)
+            this.showingLoader = show;
+            this.currLoaderName = name;
+        },
         submitForm: (e, fn) => {
             e.preventDefault();
             this.submitQuery(e.currentTarget, fn);
@@ -35,6 +43,8 @@ const app = Vue.createApp({
     //to have them in listeners inline
     computed: {
         console: () => console,
-        window: () => window
+        window: () => window,
+
+        description() { return 'Simple js ' + (this.isBtc === true ? 'btc' : this.isBtc) + ' explorer made with vue.js' },
     }
 });
